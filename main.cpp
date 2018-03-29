2c4a1d96fc428e595fd7c05f4835d285947bcc61
JPierce95 <35712472+JPierce95@users.noreply.github.com>
2018-03-29 09:36:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,25 @@
-//Author:
+//Author: Joshua E. Pierce
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-  ifstream in("example.txt");
+  ifstream inFile;
   char first = ' ', last = ' ', current;
+  vector <char> letters;
+
+  inFile.open("example.txt");
+
+  while(!(inFile.eof())){
+    inFile >> current;
+    letters.push_back(current);
+  }
+
+  inFile.close();
+
 
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
@@ -16,7 +28,7 @@ int main()
 
 
   //Then, finally, this displays the censored version:
-  cout<<first<<"**"<<last<<endl;
+  cout<<letters.at(0)<<"**"<<letters.at(letters.size() - 1)<<endl;
 
   return 0;
 }
